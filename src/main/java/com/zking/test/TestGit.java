67bc66543663143b8c1e69f718ab0c9251691f49
fantasy <1572828046@qq.com>
2017-11-13 20:10:26 +0800

    Initial commit

---
+++ src/main/java/com/zking/test/TestGit.java
@@ -0,0 +1,13 @@
+package com.zking.test;
+
+/**
+ * @outhor 言言
+ * @create 2017-11-13 19:34
+ */
+public class TestGit {
+
+    public void test(){
+        System.out.println("我与Git的第一次");
+    }
+
+}
